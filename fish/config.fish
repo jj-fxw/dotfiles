372141588304121d5e8a36321a4d9b5db2e12946
jj-fxw <108629480+jj-fxw@users.noreply.github.com>
2025-08-02 14:30:11 +0100

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,15 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    alias emacs 'emacsclient -t'
-    alias em 'emacsclient -t'
-    set XDG_CONFIG_HOME "~/.config/"
-    if not set -q TMUX
-       exec tmux
-    end
+    # Commands to run in interactive sessions can go here	
+      	# Set up the em command to run emacs (in terminal)
+      	alias em 'emacs -nw'
+	# Launch term customiser for graphics
+	oh-my-posh init fish --config ~/.config/oh-my-posh/darkblood.omp.json | source
+	# Launch tmux if tmux is not already running
+	if not set -q TMUX
+	    exec tmux
+	end
 end
 
-# Created by `pipx` on 2024-09-10 21:17:22
-set PATH $PATH /home/jj/.local/bin
+
+# Created by `pipx` on 2024-09-06 22:40:37
+set PATH $PATH /Users/jj/.local/bin
